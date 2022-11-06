195bf4d6fa8b3538aad8f16c950b281ee646c296
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-06 16:30:48 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,93 @@
-var Call=10;
-alert("hello2");
-alert(Call);
+// a=1
+// var b = --a
+// // a+b
+// console.log(b,a)//2,1
+//1,1
+//0,0
+//1,0 
+//0,1
+//1,2
+//2,1
+//0,2
+//2,3
+//2,2
+// a=5
+// b= a + ++a + a-- + ++a
+// // 5 + 6 + 6 + 6
+// console.log(b,a)//5,1
+// a=4
+// b=5
+// var c =  a +  ++b  -  --a   +  a++  +  b++
+//         // 4  + 6  -  3  + 3  + 6
+// console.log(c)
+
+// var inp = prompt("Enter Your Name","HR")
+// console.log(inp)
+
+// var inp1 = prompt("Enter Your age","20")
+// console.log(inp1)
+
+// var inp2 = prompt("Enter Your Salary","1000")
+// console.log(inp2)
+
+// var num1 =parseInt(prompt("Enter Number 1"))
+// var num2 = parseInt(prompt("Enter Number 2"))
+// var sum = num1+num2
+// // console.log('sum of a (' + num1 + " ) " + "b" + "( " + num2 +") is :"+ sum )
+// console.log("Value of a is :"+ num1)
+// console.log("Value of a is :"+ num2)
+// console.log("sum :"+ sum)
+
+
+// greater 
+// user even,odd,%
+
+// var number = "10"
+// if (number > 0) {
+//     console.log("number is positive ")
+// }
+// else if(number<0){
+//     console.log("number is negative ")
+// }
+
+// else {
+//     console.log("zero ")
+// }
+
+// var a=true
+// console.log(typeof a)
+
+// inp = parseInt(prompt("EnteR nUMBER"))
+// if(inp%2==0){
+//     console.log("number is Even")
+// }
+// else{
+//     console.log("Number iS odd")
+
+// }
+
+// document.write("<h1 style='color:red'><u>Hello World</u></h1>")
+// document.write("<h1 style='color:red'><u>Hello World</u></h1>")
+// document.write("<h1 style='color:red'><u>Hello World</u></h1>")
+
+var name1= prompt("Enter Data ")
+var qty = prompt("enter qty")
+var prod = prompt("enter pro")
+document.write(name1+"order jhgdhgehd"+qty+" "+prod)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
